d4e90ff8a6c76778fe4bc330f2aaad3f9d0930ef
redhendery <redhendery@gmail.com>
2016-08-18 13:00:08 +1200

    stub test for maps feature

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -35,3 +35,16 @@ test('should filter the list of rentals by city.', function (assert) {
     assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
   });
 });
+
+let StubMapsService = Ember.Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
